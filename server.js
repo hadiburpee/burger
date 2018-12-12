8d019c31d4e6982a5c2f995bbb623e0712d1c884
hadiburpee <hadiburpee@gmail.com>
2018-12-11 21:52:47 -0800

    set up server and one route

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 var express = require("express");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+var expBars = require("express-handlebars");
+
+app.engine("handlebars", expBars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("../burger/controllers/burgers_controllers.js");
+
+app.use(routes);
+
+app.listen(PORT, function(){
+    console.log("App now listening at http://localhost/" + PORT);
+});
